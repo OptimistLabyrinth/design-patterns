35170f8bf81f5c8ef09af7f9b6aa6f7f651c7b93
OptimistLabyrinth <keesung0705@gmail.com>
2023-01-08 22:35:29 +0900

    feat: implement template method design pattern

---
+++ src/main/java/intro/to/design/patterns/using/javalang/templatemethod/StringDisplay.java
@@ -0,0 +1,34 @@
+package intro.to.design.patterns.using.javalang.templatemethod;
+
+public class StringDisplay extends AbstractDisplay {
+    private String string;
+    private int width;
+
+    public StringDisplay(String string) {
+        this.string = string;
+        this.width = string.length();
+    }
+
+    private void printLine() {
+        System.out.print("+");
+        for (int i = 0; i < width + 2; ++i) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+    }
+
+    @Override
+    public void open() {
+        printLine();
+    }
+
+    @Override
+    public void print() {
+        System.out.println("| " + string + " |");
+    }
+
+    @Override
+    public void close() {
+        printLine();
+    }
+}
---
+++ src/main/java/intro/to/design/patterns/using/javalang/templatemethod/TemplateMethodMain.java
@@ -0,0 +1,10 @@
+package intro.to.design.patterns.using.javalang.templatemethod;
+
+public class TemplateMethodMain {
+    public static void main(String[] args) {
+        AbstractDisplay d1 = new CharDisplay('H');
+        AbstractDisplay d2 = new StringDisplay("Hello, World!");
+        d1.display();
+        d2.display();
+    }
+}
